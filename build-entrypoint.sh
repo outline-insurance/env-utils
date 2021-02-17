75485ee6810aca8cbc06fd8caa0e69f5fdc4ac14
Graham Booth <gdbooth27@gmail.com>
2021-02-16 16:07:33 -0800

    go mod stuff

--- build-entrypoint.sh
+++ build-entrypoint.sh
@@ -7,8 +7,7 @@ tar xf go1.14.linux-amd64.tar.gz
 chown -R root:root ./go
 mv go /usr/local
 
-export PATH=$PATH:/usr/local/go/bin
+# export PATH=$PATH:/usr/local/go/bin
 
 cd /tmp
-export GO111MODULE="on"
 go build 
